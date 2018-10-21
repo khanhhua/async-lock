42306a27db60e183728d4ccbde076d6f2d8f1b9d
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-10-21 09:14:39 +0800

    Fixed the lock id

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,7 +8,7 @@ describe('AsyncLock', () => {
     expect(release).to.be.a('function');
   });
 
-  it('should lock code section per module scope', async () => {
+  it('should maintain sequential order', async () => {
     let sharedVar = 0;
 
     async function expensive(nth) {
@@ -29,5 +29,36 @@ describe('AsyncLock', () => {
     ]);
 
     expect(sharedVar).to.be.equal(5);
+  });
+
+  it('should lock code section per module:line scope', async () => {
+    let sharedVar = 0;
+
+    async function sequence1(nth) {
+      const release = await lock();
+      await new Promise(resolve => setTimeout(resolve, 100));
+
+      sharedVar = nth;
+      release();
+      return nth;
+    }
+    async function sequence2(nth) {
+      const release = await lock();
+      await new Promise(resolve => setTimeout(resolve, 10));
+
+      sharedVar = nth;
+      release();
+      return nth;
+    }
+
+    await Promise.all([
+      sequence1(1),
+      sequence1(2),
+      sequence2('a'),
+      sequence2('b'),
+    ]);
+
+    // As sequence1 finishes later (200ms), sharedVar is always 2
+    expect(sharedVar).to.be.equal(2);
   });
 });
